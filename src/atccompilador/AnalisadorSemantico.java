1d96d3be1eaaa2ef5f7dca2191866d01e3b62996
Marcos Douglas <marcusdgls96@gmail.com>
2018-05-07 19:41:29 -0300

    add

--- src/atccompilador/AnalisadorSemantico.java
+++ src/atccompilador/AnalisadorSemantico.java
@@ -21,50 +21,57 @@ public class AnalisadorSemantico {
     }
 
     public List<String> analisar() {
-
-        List<String> erros = new ArrayList<>();
-        boolean disparoSe = false;
-        boolean achoEntao = false;
-        boolean achoFimSe = false;
-       
-
-        if (this.tipos.get(0) != Tipos.INICIO) {
-            erros.add("codigo não começa com palavra INICIO!\n\r");
-        }
         
-        if (this.tipos.get(this.tipos.size() - 1) != Tipos.FIM) {
-            erros.add("codigo não termina com palavra FIM!\n\r");
-        }
+        List<String> erros = new ArrayList<>();
+            boolean disparoSe = false;
+            boolean achoEntao = false;
+            boolean achoFimSe = false;
 
-        for (int i = 0; i < this.tipos.size(); i++) {
+        try {
+
             
-            if (i == this.tipos.size() - 1)
-                    break;
-            
-            if (this.tipos.get(i) == Tipos.SE) {
-                disparoSe = true;
-                if(this.tipos.get(i + 1) == Tipos.KEY_WORD){
-                    erros.add("Apos palavra SE não pode vim uma palavra reservada!");
+
+            if (this.tipos.get(0) != Tipos.INICIO) {
+                erros.add("codigo não começa com palavra INICIO!\n\r");
+            }
+
+            if (this.tipos.get(this.tipos.size() - 1) != Tipos.FIM) {
+                erros.add("codigo não termina com palavra FIM!\n\r");
+            }
+
+            for (int i = 0; i < this.tipos.size(); i++) {
+
+//            if (i == this.tipos.size() - 1)
+//                    break;
+                if (this.tipos.get(i) == Tipos.SE) {
+                    disparoSe = true;
+
+                    if (this.tipos.get(i + 1) == Tipos.KEY_WORD) {
+                        erros.add("Apos palavra SE não pode vim uma palavra reservada!");
+                    }
+                }
+                if (this.tipos.get(i) == Tipos.ENTAO) {
+                    achoEntao = true;
+                }
+                if (this.tipos.get(i) == Tipos.FIMSE) {
+                    achoFimSe = true;
                 }
             }
-            if (this.tipos.get(i) == Tipos.ENTAO) {
-                achoEntao = true;
+
+            if (disparoSe) {
+                if (!achoEntao) {
+                    erros.add("Palavra ENTAO não encontrada após SE!\n\r");
+                }
+                if (!achoFimSe) {
+                    erros.add("SE não fechado com FIMSE!\n\r");
+                }
             }
-            if (this.tipos.get(i) == Tipos.FIMSE) {
-                achoFimSe = true;
-            }
+
+            return erros;
+        } catch (Exception e) {
+            return erros;
         }
 
-        if (disparoSe) {
-            if (!achoEntao) {
-                erros.add("Palavra ENTAO não encontrada após SE!\n\r");
-            }
-            if (!achoFimSe) {
-                erros.add("SE não fechado com FIMSE!\n\r");
-            }
-        }
-        
-        return erros;
     }
 
     public List<Tipos> getTipos() {
